1670d928cd5058d9698cc430871df6723251e32e
Jose Manuel Velasco Martinez <josemanuel.velasco@globant.com>
2026-01-22 00:31:46 +0100

    test(refactor): useCharacter usa generateSlug compartido

--- apps/web/src/hooks/useCharacter.ts
+++ apps/web/src/hooks/useCharacter.ts
@@ -1,5 +1,6 @@
 import useSWR from 'swr'
 import type { Character } from '../types/character'
+import { generateSlug } from '../utils/slugUtils'
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json())
 
@@ -10,8 +11,7 @@ export function useCharacter(slug: string) {
   )
 
   const character = characters?.find((character) => {
-    const characterSlug = character.name.toLowerCase().replace(/\s+/g, '-')
-    return characterSlug === slug
+    return generateSlug(character.name) === slug
   })
 
   return {
