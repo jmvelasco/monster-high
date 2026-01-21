2cc6c68dda3d9edc966b964e09b52c9689167229
Jose Manuel Velasco Martinez <josemanuel.velasco@globant.com>
2026-01-21 23:30:00 +0100

    test(green): muestra loading state mientras carga

--- apps/web/src/pages/CharacterListPage.tsx
+++ apps/web/src/pages/CharacterListPage.tsx
@@ -1,3 +1,11 @@
+import { useCharacters } from '../hooks/useCharacters';
+
 export function CharacterListPage() {
+  const { isLoading } = useCharacters();
+
+  if (isLoading) {
+    return <div>Cargando personajes...</div>;
+  }
+
   return <div>CharacterListPage</div>;
 }
