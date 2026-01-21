d2248ffb037cd26fe0532d5946b5282042b11502
Jose Manuel Velasco Martinez <josemanuel.velasco@globant.com>
2026-01-22 00:47:50 +0100

    test(green): CharacterDetail muestra todos los campos de technicalInfo presentes

--- apps/web/src/components/character/CharacterDetail.tsx
+++ apps/web/src/components/character/CharacterDetail.tsx
@@ -10,6 +10,28 @@ export function CharacterDetail({ character }: CharacterDetailProps) {
   return (
     <div>
       <img src={imageSrc} alt={character.name} />
+      <div>
+        {character.technicalInfo.edad && (
+          <div>
+            <span>Edad:</span> <span>{character.technicalInfo.edad}</span>
+          </div>
+        )}
+        {character.technicalInfo.sexo && (
+          <div>
+            <span>Sexo:</span> <span>{character.technicalInfo.sexo}</span>
+          </div>
+        )}
+        {character.technicalInfo.ocupacion && (
+          <div>
+            <span>Ocupación:</span> <span>{character.technicalInfo.ocupacion}</span>
+          </div>
+        )}
+        {character.technicalInfo.mascota && (
+          <div>
+            <span>Mascota:</span> <span>{character.technicalInfo.mascota}</span>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
