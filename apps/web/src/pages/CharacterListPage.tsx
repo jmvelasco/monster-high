754300b8c15e7a0154474a9d0bd445b88381c32d
Jose Manuel Velasco Martinez <josemanuel.velasco@globant.com>
2026-01-21 23:34:33 +0100

    test(green): muestra error state si fetch falla

--- apps/web/src/pages/CharacterListPage.tsx
+++ apps/web/src/pages/CharacterListPage.tsx
@@ -2,10 +2,14 @@ import { useCharacters } from '../hooks/useCharacters';
 import { CharacterGrid } from '../components/character/CharacterGrid';
 
 export function CharacterListPage() {
-  const { data, isLoading } = useCharacters();
+  const { data, error, isLoading } = useCharacters();
 
   if (isLoading) {
     return <div>Cargando personajes...</div>;
+  }
+
+  if (error) {
+    return <div>Error al cargar personajes</div>;
   }
 
   return <CharacterGrid characters={data || []} />;
