9c9d5891c513082b78b9096dbadcd567b2e50946
Jose Manuel Velasco Martinez <josemanuel.velasco@globant.com>
2026-01-22 00:56:46 +0100

    test(green): CharacterDetailPage muestra loading state mientras carga

--- apps/web/src/pages/CharacterDetailPage.tsx
+++ apps/web/src/pages/CharacterDetailPage.tsx
@@ -1,3 +1,23 @@
+import { useParams } from 'react-router-dom'
+import { useCharacter } from '../hooks/useCharacter'
+import { CharacterDetail } from '../components/character/CharacterDetail'
+
 export function CharacterDetailPage() {
-  return <div>CharacterDetailPage</div>
+  const { slug } = useParams<{ slug: string }>()
+  const { data: character, isLoading, error } = useCharacter(slug || '')
+
+  if (isLoading) {
+    return <div>Cargando...</div>
+  }
+
+  if (error) {
+    return <div>Error al cargar personaje</div>
+  }
+
+  if (!character) {
+    return <div>Personaje no encontrado</div>
+  }
+
+  return <CharacterDetail character={character} />
 }
+
