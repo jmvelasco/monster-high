07e65bec5ca1861a7f9bbe30423345310932b460
Jose Manuel Velasco Martinez <josemanuel.velasco@globant.com>
2026-01-21 21:59:34 +0100

    test(red): muestra nombre del personaje

---
+++ apps/web/src/components/character/CharacterCard.tsx
@@ -0,0 +1,10 @@
+import type { Character } from '../../types/character'
+
+interface CharacterCardProps {
+  character: Character
+  variant: 'list' | 'favorite'
+}
+
+export function CharacterCard() {
+  return null
+}
