f0aea075a984409abe49b7b9cb8a5a74a12c4293
Jose Manuel Velasco Martinez <josemanuel.velasco@globant.com>
2026-01-22 22:25:13 +0100

    test(green): agrega useIsMobile hook y button condicional

--- apps/web/src/components/Header.tsx
+++ apps/web/src/components/Header.tsx
@@ -1,9 +1,13 @@
 import { NavLink } from 'react-router-dom'
+import { useIsMobile } from '../hooks/useIsMobile'
 
 export function Header() {
+  const isMobile = useIsMobile()
+
   return (
     <header>
       <h1>Monster High</h1>
+      {isMobile && <button>Abrir menú</button>}
       <NavLink to="/characters">Todos los Personajes</NavLink>
       <NavLink to="/favorites">Favoritos</NavLink>
     </header>
