9f1caee1bedb88592804ce890d0292d2d351d1d2
Jose Manuel Velasco Martinez <josemanuel.velasco@globant.com>
2026-05-13 00:54:35 +0200

    test(green): creates a friend group

--- apps/web/src/friends/application/CreateFriendGroupUseCase.ts
+++ apps/web/src/friends/application/CreateFriendGroupUseCase.ts
@@ -1,7 +1,11 @@
+import { FriendGroup } from '../../domain/friends/FriendGroup'
 import type { FriendGroupRepository } from '../../domain/friends/FriendGroupRepository'
 
 export class CreateFriendGroupUseCase {
   constructor(private readonly repository: FriendGroupRepository) {}
 
-  async execute(name: string): Promise<void> {}
+  async execute(name: string): Promise<void> {
+    const group = FriendGroup.create({ id: crypto.randomUUID(), name })
+    await this.repository.save(group)
+  }
 }
