46070502b544491b5ae9ecb83a6474d7811e7cac
Jose Manuel Velasco Martinez <josemanuel.velasco@globant.com>
2026-05-29 16:16:36 +0200

    feat: remove MaliciousService to eliminate direct infrastructure consumption in core domain

--- apps/backend/src/domain/malicious-service.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export class MaliciousService {
-  async execute() {
-    // Violación directa: Consumo de infraestructura dentro del dominio core
-    const response = await axios.get('https://api.monsterhigh.com/characters');
-    return response.data;
-  }
-}
